31f384133fc58e68011a70be5ee8d1f415e878f1
isulnethila <139636196+isulnethila@users.noreply.github.com>
2025-03-03 23:19:04 +0530

    form update with state variables

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { useState } from "react";
-
+import Head from 'next/head';
 
 
 export default function Home() {
@@ -12,15 +12,25 @@ export default function Home() {
   const [targetCurrency,setTargetCurrency]=useState("");
   const [amountInSourceCurrency,setAmountInSourceCurrency]=useState(0);
   const [amountInTargetCurrency,setAmountIntargetCurrency]=useState(0);
+
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    console.log(
+      date,
+      sourceCurrency,
+      targetCurrency,
+      amountInSourceCurrency
+    )
+  }
   return (
     <div>
-      <head>
+      <Head>
         <title>converter</title>
         <style>
           @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;800&display=swap');
         </style>
 
-      </head>
+      </Head>
       <main className="font-poppins p-10 ">
         <h1 className=" text-green-500 text-5xl font-bold text-center  lg:mx-32 ">Convert Your Currencies Today</h1>
         <p className="lg:mx-32 py-8 "> Welcome to "Convert Your Currencies Today"! This application allows you
@@ -30,10 +40,11 @@ export default function Home() {
           help.</p>
         <div className="mt-5 flex items-center justify-center flex-col">
           <section className="w-full lg:w-1/2">
-            <form>
+            <form onSubmit={handleSubmit}>
               <div className="mb-4">
                 <label htmlFor={date} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date</label>
                 <input type="Date"
+                onChange={(e)=>setDate(e.target.value)}
                  id={date}
                  name={date} 
                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
@@ -42,7 +53,11 @@ export default function Home() {
               <div className="mb-4">
                 <label htmlFor={sourceCurrency} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Source Currency</label>
 
-                <select name={sourceCurrency} id={sourceCurrency} value={sourceCurrency}className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <select 
+                onChange={(e)=>setSourceCurrency(e.target.value)}
+                name={sourceCurrency} 
+                id={sourceCurrency} 
+                value={sourceCurrency}className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                   <option>select a source Currency</option>
 
                 </select>
@@ -50,7 +65,11 @@ export default function Home() {
 
               <div className="mb-4">
                 <label htmlFor={sourceCurrency} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Target Currency</label>
-                <select name={targetCurrency} value={targetCurrency} id={targetCurrency}className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <select name={targetCurrency} 
+                onChange={(e)=>setTargetCurrency(e.target.value)}
+                value={targetCurrency} 
+                id={targetCurrency}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                   <option>select a target Currency</option>
 
                 </select>
@@ -58,7 +77,11 @@ export default function Home() {
 
               <div className="mb-4">
                 <label htmlFor={amountInSourceCurrency} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Amount in source Currency</label>
-                <input type="text" id={amountInSourceCurrency} name={amountInSourceCurrency} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Amount in source currency" required />
+                <input 
+                onChange={(e)=>setAmountInSourceCurrency(e.target.value)}
+                type="text" 
+                id={amountInSourceCurrency}
+                name={amountInSourceCurrency} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Amount in source currency" required />
               </div>
 
               <button className="bg-green-700 hover:bg-green-900 text-white  px-4 py-2 my-4 rounded-lg">Get the target currency</button>
